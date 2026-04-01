848b07023ba7048e8a417ab1bdb2ae4ccf812ddf
amisttt <166504069+amisttt@users.noreply.github.com>
2026-04-02 02:17:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ function setSavingsForMonth(mk, value) {
   _saveSavingsMap(map);
 }
 
-const DEFAUKT_INCOME = 1000;
+const DEFAULT_INCOME = 1000;
 /** Load the full { "YYYY-MM": number } map from storage */
 function _loadIncomeMap() {
   try {
